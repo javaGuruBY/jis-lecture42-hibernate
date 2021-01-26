d14bb6bb0b81ad23dcea07a5b5f9acbb62862d3a
max-java <6666350@gmail.com>
2021-01-27 02:07:33 +0300

    adds entityManager non xml config

--- annotConfig/src/main/java/by/jrr/dao/HibernateEmUtil.java
+++ annotConfig/src/main/java/by/jrr/dao/HibernateEmUtil.java
@@ -1,93 +1,160 @@
 package by.jrr.dao;
 
-import by.jrr.bean.*;
-import org.hibernate.SessionFactory;
-import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
-import org.hibernate.cfg.Configuration;
-import org.hibernate.cfg.Environment;
-import org.hibernate.dialect.Oracle12cDialect;
 import org.hibernate.jpa.HibernatePersistenceProvider;
-import org.hibernate.service.ServiceRegistry;
 
+import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.SharedCacheMode;
+import javax.persistence.ValidationMode;
+import javax.persistence.spi.ClassTransformer;
 import javax.persistence.spi.PersistenceUnitInfo;
+import javax.persistence.spi.PersistenceUnitTransactionType;
+import javax.sql.DataSource;
 import java.io.FileInputStream;
+import java.io.IOException;
 import java.io.InputStream;
-import java.util.Properties;
+import java.io.UncheckedIOException;
+import java.net.URL;
+import java.util.*;
+
+import static org.hibernate.cfg.AvailableSettings.SHOW_SQL;
+
 
 public class HibernateEmUtil {
+//    https://stackoverflow.com/questions/1989672/create-jpa-entitymanager-without-persistence-xml-configuration-file
 //todo: tbd
     private static EntityManagerFactory entityManagerFactory;
 
-    public static EntityManagerFactory getEntityManagerFactory() {
-//        if (entityManagerFactory == null) {
-//            try {
-//                entityManagerFactory =new HibernatePersistenceProvider()
-//                        .createContainerEntityManagerFactory(archiverPersistenceUnitInfo(),ImmutableMap.
-//
-//                        <String, Object> builder() .
-//
-//                        put(JPA_JDBC_DRIVER, JDBC_DRIVER) .
-//
-//                        put(JPA_JDBC_URL, JDBC_URL) .
-//
-//                        put(DIALECT, Oracle12cDialect .class) .
-//
-//                        put(HBM2DDL_AUTO, CREATE) .
-//
-//                        put(SHOW_SQL, false) .
-//
-//                        put(QUERY_STARTUP_CHECKING, false) .
-//
-//                        put(GENERATE_STATISTICS, false) .
-//
-//                        put(USE_REFLECTION_OPTIMIZER, false) .
-//
-//                        put(USE_SECOND_LEVEL_CACHE, false) .
-//
-//                        put(USE_QUERY_CACHE, false) .
-//
-//                        put(USE_STRUCTURED_CACHE, false) .
-//
-//                        put(STATEMENT_BATCH_SIZE, 20) .
-//
-//                        build());entityManager =entityManagerFactory.createEntityManager();
-//
-//                Properties settings = loadProperties();
-//
-//                settings.setProperty(Environment.DRIVER, "com.mysql.jdbc.Driver");
-//                settings.setProperty(Environment.DIALECT, "org.hibernate.dialect.MySQL5Dialect");
-//                settings.setProperty(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
-//
-////                settings.put(Environment.USE_SECOND_LEVEL_CACHE, true);
-////                settings.put(Environment.CACHE_REGION_FACTORY, "org.hibernate.cache.ehcache.EhCacheRegionFactory");
-////                settings.put(Environment.USE_QUERY_CACHE, true);
-//
-//                Configuration configuration = new Configuration();
-//                configuration.setProperties(settings);
-//
-//                configuration.addAnnotatedClass(Human.class);
-//                configuration.addAnnotatedClass(Student.class);
-//                configuration.addAnnotatedClass(StudentPIIData.class);
-//                configuration.addAnnotatedClass(Alumnus.class);
-//                configuration.addAnnotatedClass(AlumnusPIIData.class);
-//                configuration.addAnnotatedClass(Mentor.class);
-//                configuration.addAnnotatedClass(MentorPIIData.class);
-//                configuration.addAnnotatedClass(User.class);
-//
-//
-//                ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
-//                        .applySettings(configuration.getProperties()).build();
-//
-//                sessionFactory = configuration.buildSessionFactory(serviceRegistry);
-//            } catch (Throwable ex) {
-//                // Make sure you log the exception, as it might be swallowed
-//                System.err.println("Initial SessionFactory creation failed." + ex);
-//                throw new ExceptionInInitializerError(ex);
-//            }
-//        }
-        return entityManagerFactory;
+    public static void createEntityManagerFactory() {
+        if (entityManagerFactory == null) {
+            Map<String, Object> settings = new HashMap(loadProperties());
+//            additional settings
+            settings.put(SHOW_SQL, true);
+//            settings.put(QUERY_STARTUP_CHECKING, false);
+//            settings.put(GENERATE_STATISTICS, false);
+//            settings.put(USE_REFLECTION_OPTIMIZER, false);
+//            settings.put(USE_SECOND_LEVEL_CACHE, false);
+//            settings.put(USE_QUERY_CACHE, false);
+//            settings.put(USE_STRUCTURED_CACHE, false);
+//            settings.put(STATEMENT_BATCH_SIZE, 20);
+
+            entityManagerFactory = new HibernatePersistenceProvider()
+                    .createContainerEntityManagerFactory(archiverPersistenceUnitInfo(),settings);
+        }
+    }
+
+    public static EntityManager getEntityManager() {
+         if(entityManagerFactory == null) {
+            createEntityManagerFactory();
+        }
+        return entityManagerFactory.createEntityManager();
+    }
+
+    private static Properties loadProperties() {
+        Properties properties = new Properties();
+
+        try (InputStream in = new FileInputStream("src/main/resources/jis4.properties")) {
+            properties.load(in);
+        } catch (Exception ex) {
+            System.out.println("Cannot find properties");
+            return null;
+        }
+        return properties;
+    }
+
+    private static PersistenceUnitInfo archiverPersistenceUnitInfo() {
+        return new PersistenceUnitInfo() {
+            @Override
+            public String getPersistenceUnitName() {
+                return "ApplicationPersistenceUnit";
+            }
+
+            @Override
+            public String getPersistenceProviderClassName() {
+                return "org.hibernate.jpa.HibernatePersistenceProvider";
+            }
+
+            @Override
+            public PersistenceUnitTransactionType getTransactionType() {
+                return PersistenceUnitTransactionType.RESOURCE_LOCAL;
+            }
+
+            @Override
+            public DataSource getJtaDataSource() {
+                return null;
+            }
+
+            @Override
+            public DataSource getNonJtaDataSource() {
+                return null;
+            }
+
+            @Override
+            public List<String> getMappingFileNames() {
+                return Collections.emptyList();
+            }
+
+            @Override
+            public List<URL> getJarFileUrls() {
+                try {
+                    return Collections.list(this.getClass()
+                            .getClassLoader()
+                            .getResources(""));
+                } catch (IOException e) {
+                    throw new UncheckedIOException(e);
+                }
+            }
+
+            @Override
+            public URL getPersistenceUnitRootUrl() {
+                return null;
+            }
+
+            @Override
+            public List<String> getManagedClassNames() {
+                return Collections.emptyList();
+            }
+
+            @Override
+            public boolean excludeUnlistedClasses() {
+                return false;
+            }
+
+            @Override
+            public SharedCacheMode getSharedCacheMode() {
+                return null;
+            }
+
+            @Override
+            public ValidationMode getValidationMode() {
+                return null;
+            }
+
+            @Override
+            public Properties getProperties() {
+                return new Properties();
+            }
+
+            @Override
+            public String getPersistenceXMLSchemaVersion() {
+                return null;
+            }
+
+            @Override
+            public ClassLoader getClassLoader() {
+                return null;
+            }
+
+            @Override
+            public void addTransformer(ClassTransformer transformer) {
+
+            }
+
+            @Override
+            public ClassLoader getNewTempClassLoader() {
+                return null;
+            }
+        };
     }
 }
-
 
---
+++ annotConfig/src/test/java/by/jrr/dao/HibernateEmUtilTest.java
@@ -0,0 +1,13 @@
+package by.jrr.dao;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HibernateEmUtilTest {
+
+    @Test
+    void getEntityManager() {
+        HibernateEmUtil.getEntityManager();
+    }
+}
